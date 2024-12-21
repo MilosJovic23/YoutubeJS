03910b0dab9dd59c58f4b61bc786bdb11ec7b0d6
MilosJovic23 <milos.mj992@gmail.com>
2024-12-21 13:27:23 +0100

    * few changes

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 
 
+    const shoppingList = ["bread", "milk"];
 
+    shoppingList.push("newspaper", "yogurt");
+    const shoppingList2 = ["juice", "pistachio"];
+    shoppingList.push(...shoppingList2);
 
-const shoppingList = ["bread", "milk"];
-
-
-shoppingList.push("newspaper", "yogurt");
-const shoppingList2 = ["juice", "pistachio"];
-shoppingList.push(...shoppingList2);
-
-console.log(shoppingList);
+    console.log(shoppingList);
